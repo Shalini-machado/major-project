1c7ae092062354b6c6b6cc29ad01e2836bcffc19
Shalini-machado <94559844+Shalini-machado@users.noreply.github.com>
2023-03-19 16:00:28 +0530

    Add files via upload

---
+++ ChatApp/login.php
@@ -0,0 +1,29 @@
+<?php include_once "header.php"; ?>
+    <body>
+        <h1><<- ʀᴇꜰᴇʀ ᴍᴇ ->></h1>
+        <div class="wrapper">
+            <section class="form login">
+                <header>Login</header>
+                <form action="#" autocomplete="off">
+                    <div class="error-txt"></div>
+                    <div class="field input">
+                            <label>Email Addresss</label>
+                            <input type="text" name="email" placeholder="Enter your email" required>
+                        </div>
+                        <div class="field input">
+                            <label>Password</label>
+                            <input type="password" name="password" placeholder="Enter your password" required>
+                            <i class="fas fa-eye"></i>
+                        </div>
+                        <div class="field button">
+                            <input type="submit" value="Login">
+                        </div>
+                </form>
+                <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+            </section>
+        </div>
+
+        <script src="javascript/pass-show-hide.js"></script>
+        <script src="javascript/login.js"></script>
+    </body>
+</html>
